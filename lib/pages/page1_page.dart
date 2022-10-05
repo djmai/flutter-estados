96a2f5a4d3eadd8ff7fd4c5943b1da2143f61a37
Miguel Martínez <mai.djmai@gmail.com>
2022-10-05 16:46:33 -0500

    Provider listo

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -1,19 +1,37 @@
-import 'dart:ffi';
-
+import 'package:estados_app/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:estados_app/services/usuario_service.dart';
 
 class Pagina1Page extends StatelessWidget {
   const Pagina1Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final UsuarioService usuarioService = Provider.of<UsuarioService>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina 1'),
+        title: const Text('Página 1'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: !usuarioService.existeUsuario
+                ? null
+                : () {
+                    usuarioService.removerUsuario();
+                  },
+          ),
+        ],
       ),
-      body: InformacionUsuario(),
+      body: usuarioService.existeUsuario
+          ? InformacionUsuario(usuario: usuarioService.usuario)
+          : const Center(
+              child: Text('No hay información del usuario'),
+            ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.accessibility_new),
+        child: const Icon(Icons.accessibility_new),
         onPressed: () => Navigator.pushNamed(context, 'pagina2'),
       ),
     );
@@ -21,26 +39,33 @@ class Pagina1Page extends StatelessWidget {
 }
 
 class InformacionUsuario extends StatelessWidget {
+  final Usuario usuario;
+
+  const InformacionUsuario({super.key, required this.usuario});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: double.infinity,
       width: double.infinity,
-      padding: EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('General',
+          const Text('General',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          Divider(),
-          ListTile(title: Text('Nombre: ')),
-          ListTile(title: Text('Edad: ')),
-          Text('Profesiones',
+          const Divider(),
+          ListTile(title: Text('Nombre: ${usuario.nombre}')),
+          ListTile(title: Text('Edad: ${usuario.edad}')),
+          const Text('Profesiones',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          Divider(),
-          ListTile(title: Text('Profersion 1')),
-          ListTile(title: Text('Profersion 1')),
-          ListTile(title: Text('Profersion 1')),
+          const Divider(),
+          ...usuario.profesiones!
+              .map((profesion) => ListTile(
+                    title: Text(profesion),
+                  ))
+              .toList()
+          // const ListTile(title: Text('Profersion 1')),
         ],
       ),
     );
--- lib/pages/page2_page.dart
+++ lib/pages/page2_page.dart
@@ -1,32 +1,58 @@
+import 'package:estados_app/models/usuario.dart';
+import 'package:estados_app/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Pagina2Page extends StatelessWidget {
   const Pagina2Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final UsuarioService usuarioService = Provider.of<UsuarioService>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina 2'),
+        title: !usuarioService.existeUsuario
+            ? Text('Página 2')
+            : Text(usuarioService.usuario.nombre),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             MaterialButton(
-              child: Text('Establecer Usuario', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              onPressed: () {
+                final user =
+                    Usuario(nombre: 'Miguel Martínez', edad: 30, profesiones: [
+                  'FullStack Developer',
+                  'Tester Developer',
+                ]);
+                usuarioService.usuario = user;
+              },
+              child: const Text('Establecer Usuario',
+                  style: TextStyle(color: Colors.white)),
             ),
             MaterialButton(
-              child: Text('Cambiar edad', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              disabledColor: Colors.blueGrey,
+              onPressed: !usuarioService.existeUsuario
+                  ? null
+                  : () => usuarioService.cambiarEdad(35),
+              child: const Text('Cambiar edad',
+                  style: TextStyle(color: Colors.white)),
             ),
             MaterialButton(
-              child: Text('Añadir profesion', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              disabledColor: Colors.blueGrey,
+              // onPressed: !usuarioService.existeUsuario ? null : () {
+              //   usuarioService.agregarProfesion();
+              // },
+              onPressed: !usuarioService.existeUsuario
+                  ? null
+                  : usuarioService.agregarProfesion,
+              child: const Text('Añadir profesión',
+                  style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
